05e34994703cfacf545def20e4f6c1da6d858ef2
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2023-08-22 22:29:48 -0400

    Changed typeface for Iosevka

--- demo-app/src/main/java/dev/gabrieldrn/konstellationdemo/AppModule.kt
+++ demo-app/src/main/java/dev/gabrieldrn/konstellationdemo/AppModule.kt
@@ -20,7 +20,7 @@ val appModule = module {
     viewModel { parameters -> LineChartDemoViewModel(parameters.get()) }
     single(QF_MAIN_TEXT_STYLE) {
         TextDrawStyle(
-            typeface = ResourcesCompat.getFont(androidContext(), R.font.inter_medium)!!
+            typeface = ResourcesCompat.getFont(androidContext(), R.font.iosevka_ss08_ex)!!
         )
     }
 }
--- demo-app/src/main/java/dev/gabrieldrn/konstellationdemo/ui/theme/Type.kt
+++ demo-app/src/main/java/dev/gabrieldrn/konstellationdemo/ui/theme/Type.kt
@@ -3,19 +3,44 @@ package dev.gabrieldrn.konstellationdemo.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import dev.gabrieldrn.konstellationdemo.R
 
- private val InterFamily = FontFamily(
-    Font(R.font.inter_thin, weight = FontWeight.Thin),
-    Font(R.font.inter_extralight, weight = FontWeight.ExtraLight),
-    Font(R.font.inter_light, weight = FontWeight.Light),
-    Font(R.font.inter_regular),
-    Font(R.font.inter_medium, weight = FontWeight.Medium),
-    Font(R.font.inter_semibold, weight = FontWeight.SemiBold),
-    Font(R.font.inter_bold, weight = FontWeight.Bold),
-    Font(R.font.inter_extrabold, weight = FontWeight.ExtraBold),
-    Font(R.font.inter_black, weight = FontWeight.Black),
+/**
+ * Iosevka SS08 Extended font family.
+ */
+private val IosevkaFamily = FontFamily(
+    Font(R.font.iosevka_ss08_ex_thin, weight = FontWeight.Thin),
+    Font(R.font.iosevka_ss08_ex_thinitalic, weight = FontWeight.Thin, style = FontStyle.Italic),
+    Font(R.font.iosevka_ss08_ex_extralight, weight = FontWeight.ExtraLight),
+    Font(
+        R.font.iosevka_ss08_ex_extralightitalic,
+        weight = FontWeight.ExtraLight,
+        style = FontStyle.Italic
+    ),
+    Font(R.font.iosevka_ss08_ex_light, weight = FontWeight.Light),
+    Font(R.font.iosevka_ss08_ex_lightitalic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.iosevka_ss08_ex),
+    Font(R.font.iosevka_ss08_ex_italic, style = FontStyle.Italic),
+    Font(R.font.iosevka_ss08_ex_medium, weight = FontWeight.Medium),
+    Font(R.font.iosevka_ss08_ex_mediumitalic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.iosevka_ss08_ex_semibold, weight = FontWeight.SemiBold),
+    Font(
+        R.font.iosevka_ss08_ex_semibolditalic,
+        weight = FontWeight.SemiBold,
+        style = FontStyle.Italic
+    ),
+    Font(R.font.iosevka_ss08_ex_bold, weight = FontWeight.Bold),
+    Font(R.font.iosevka_ss08_ex_bolditalic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.iosevka_ss08_ex_extrabold, weight = FontWeight.ExtraBold),
+    Font(
+        R.font.iosevka_ss08_ex_extrabolditalic,
+        weight = FontWeight.ExtraBold,
+        style = FontStyle.Italic
+    ),
+    Font(R.font.iosevka_ss08_ex_heavy, weight = FontWeight.Black),
+    Font(R.font.iosevka_ss08_ex_heavyitalic, weight = FontWeight.Black, style = FontStyle.Italic),
 )
 
 /**
@@ -24,59 +49,59 @@ import dev.gabrieldrn.konstellationdemo.R
 val Typography = Typography().run {
     copy(
         displayLarge = displayLarge.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.Light
         ),
         displayMedium = displayLarge.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.Light
         ),
         displaySmall = displayLarge.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.Light
         ),
 
         headlineLarge = headlineLarge.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.SemiBold
         ),
         headlineMedium = headlineMedium.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.SemiBold
         ),
         headlineSmall = headlineSmall.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.SemiBold
         ),
 
         titleLarge = titleLarge.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.SemiBold
         ),
         titleMedium = titleMedium.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.SemiBold
         ),
         titleSmall = titleSmall.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.SemiBold
         ),
 
         bodyLarge = bodyLarge.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.Medium
         ),
         bodyMedium = bodyMedium.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.Medium
         ),
         bodySmall = bodySmall.copy(
-            fontFamily = InterFamily,
+            fontFamily = IosevkaFamily,
             fontWeight = FontWeight.Medium
         ),
 
-        labelLarge = labelLarge.copy(fontFamily = InterFamily),
-        labelMedium = labelMedium.copy(fontFamily = InterFamily),
-        labelSmall = labelSmall.copy(fontFamily = InterFamily)
+        labelLarge = labelLarge.copy(fontFamily = IosevkaFamily),
+        labelMedium = labelMedium.copy(fontFamily = IosevkaFamily),
+        labelSmall = labelSmall.copy(fontFamily = IosevkaFamily)
     )
 }
